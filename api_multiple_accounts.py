e386abb4f306dfbeea35e700f702ccdf84d5fb30
carlitoselmago <ccarbonell@gmail.com>
2021-09-21 10:02:46 +0200

    Create api_multiple_accounts.py

---
+++ api_multiple_accounts.py
@@ -0,0 +1,16 @@
+
+--1--
+
+#API USER
+consumer_key='sqCZ4EKznfmKwxKw5y9DLexFD'
+consumer_secret='tIRaumL8cHX3R6d3CRAHxWePIfENfl87Uj0mn570mUTOp2zQPv'
+access_token='1436664768425758720-oaT9rdnSWPmddZVR2vFUifUUIVb17i'
+access_token_secret='UkiFinhCS152lwbsyOQeRipwkdWHH2VQCmUF43PZ3COdJ'
+
+--2--
+
+#API USER
+consumer_key='kL3WPfruMjHisSNCA7tWjokbg'
+consumer_secret='dAPbSylKdLnZxzRFx4zxmgobzEA0tgehXbX1IUUoL7AKonADhb '
+access_token='1440219543087894533-kpRXf5sE4KCGBmZyhP1XrPk8hqfuam'
+access_token_secret='OsIRIE8V6gt6MrbRSk5Hwhw7vBzNo4vnlUhWWQVpm8tCG'
